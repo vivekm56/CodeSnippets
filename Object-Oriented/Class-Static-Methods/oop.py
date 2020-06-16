bbf190bfc1ca89ed9682ef0775d43bc0f53d0025
vicky rocks <mudgalv56@gmail.com>
2020-06-17 02:35:04 +0530

    concept of class methods and static methods

---
+++ Object-Oriented/Class-Static-Methods/oop.py
@@ -0,0 +1,60 @@
+class Employee:
+    
+    numOfEmployees = 0
+    raiseAmount = 1.04
+
+    def __init__(self, firstName, lastName, salary):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.salary = salary
+        self.mailId = (firstName + lastName + '@company.com').lower()
+
+        Employee.numOfEmployees += 1
+
+    def increment(self):
+        self.salary = int(self.salary * self.raiseAmount) #'self.raiseAmount' can be changed by 'Employee.raiseAmount', if you want to keep value unchangeable
+
+
+    @classmethod                        #It can be used as an alternative constructor
+    def set_raise_amt(cls, amount):     #It can be called a class instead of an Instance
+        cls.raiseAmount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        firstName, lastName, salary = emp_str_1.split('-')
+        return (firstName, lastName, salary)
+
+    @staticmethod                       #Normal Method or function
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6 :
+            return False
+        return True
+
+
+emp_1 = Employee('Mark', 'Bucher', 50000)
+emp_2 = Employee('Eddiee', 'Paul', 60000)
+
+Employee.set_raise_amt(1.05)
+print(emp_1.raiseAmount)
+print(emp_2.raiseAmount)
+print(Employee.raiseAmount)
+
+# Employee.set_raise_amt(1.06)
+
+
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jane-Doe-90000'
+
+
+# new_emp_1 = Employee(firstName, lastName, salary)
+new_emp_1 = Employee.from_string(emp)
+
+print(new_emp_1.mailId)
+print(new_emp_1.salary)
+
+import datetime
+
+my_date = datetime.date(2020,6,17)
+
+print(Employee.is_workday(my_date))
